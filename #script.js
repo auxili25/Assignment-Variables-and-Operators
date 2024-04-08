f815db92189da96eee5a6a6320d200ae3a253184
auxiliadora <maldonadogelvez@gmail.com>
2024-04-08 19:57:51 -0400

    update-exercises

---
+++ #script.js
@@ -0,0 +1,18 @@
+console.log(23 + 97);
+
+console.log(25+27+8+26+12+23);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+console.log(9*a);
+
+let b = (7 * a);
+console.log(b);
+
+let MAX = 57;
+console.log(MAX);
+let actual = (MAX - 13);
+console.log(actual);
+let percentege = (actual/MAX);
